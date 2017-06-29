ad8b241bc17cbc5fa7c87e65702f1b6e9c58a2b3
Alain Chiasson <alain@chiasson.org>
2017-06-29 11:17:49 -0400

    Moved confgis to /etc and added tests to verify if it was created.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,60 +1,54 @@
 import os
 
 import testinfra.utils.ansible_runner
+import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_redis_data_path(host):
-    d = host.file("/data")
 
-    assert d.is_directory
-
-
+# Verify that package was installed
 def test_redis_installed(host):
     redis = host.package("redis")
 
     assert redis.is_installed
 
+# define the array of ports - base the configs on.
 
-def test_cluster_test_dir(host):
-    f = host.file('/home/vagrant/cluster-test')
+ports = [
+    ("7000"),
+    ("7001"),
+    ("7002"),
+    ("7003"),
+    ("7004"),
+    ("7005"),
+    ("7006"),
+    ("7007"),
+]
 
-    assert f.exists
+# Verify the datapath
+def test_redis_data_path(host):
+    d = host.file("/var/lib/redis")
 
-
-def test_redis_subdirs(host):
-    f = host.file('/home/vagrant/cluster-test/7000')
-
-    assert f.exists
-
-
-def test_redis_subdirs(host):
-    f = host.file('/home/vagrant/cluster-test/7001')
-
-    assert f.exists
-
-
-def test_redis_subdirs(host):
-    f = host.file('/home/vagrant/cluster-test/7002')
-
-    assert f.exists
+    assert d.is_directory
 
 
-def test_redis_subdirs(host):
-    f = host.file('/home/vagrant/cluster-test/7003')
+@pytest.mark.parametrize("port", ports)
+def test_redis_subdirs(host, port):
+
+    f = host.file("/var/lib/redis/" + port)
 
     assert f.exists
+    assert f.is_directory
 
+# Verify that the configs are correct
+@pytest.mark.parametrize("port", ports)
+def test_redis_subdirs(host, port):
 
-def test_redis_subdirs(host):
-    f = host.file('/home/vagrant/cluster-test/7004')
-
-    assert f.exists
-
-
-def test_redis_subdirs(host):
-    f = host.file('/home/vagrant/cluster-test/7005')
+    f = host.file("/etc/redis_" + port + ".conf")
 
     assert f.exists
+    assert f.is_file
+    # The following does not work as expected. Need to find out why
+    assert f.contains("port " + port)
