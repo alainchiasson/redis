a845ea9be356bab617f80ccc316798d7ce41aed8
Alain Chiasson <alain@chiasson.org>
2017-07-02 07:08:33 -0400

    Modified redis-tris with non interactive mode for cluster creation. added j2 filter for ip-port combination. multi-node testing adds dhcp allocation on private network. Redis conf is made insecure.

---
+++ filter_plugins/collections.py
@@ -0,0 +1,19 @@
+
+def arraypermute(collection, key):
+
+    '''
+    Returns all combinations of collection and key
+    '''
+
+    return [ str(i) + str(j) for i in collection for j in key ]
+
+
+class FilterModule(object):
+    '''
+    custom jinja2 filters for working with collections
+    '''
+
+    def filters(self):
+        return {
+            'arraypermute': arraypermute
+        }
